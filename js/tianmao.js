67f5fe38cd2faf71055a0da2fc4d2a0e9275fef2
stephen-zm <1694349472@qq.com>
2018-10-08 18:13:49 +0800

    add

---
+++ js/tianmao.js
@@ -0,0 +1,74 @@
+window.onload=function(){
+    now=0;
+    next=0;
+    let lia=document.querySelectorAll(".ban li")
+    let lib=document.querySelectorAll(".nav li")
+    function move(){
+        next++;
+        if(next>lia.length-1){
+            next=0;
+        }
+        lia[next].style.zIndex=15;
+        lib[next].classList.add("hot");
+        lia[now].style.zIndex=0;
+        lib[now].classList.remove("hot");
+        now=next;
+    }
+    let t=setInterval(move,2000)
+    for(let j=0;j<lib.length;j++){
+        lib[j].onmouseenter=function(){
+            if(j==now){
+                return;
+            }
+            else if(j>now){
+                lia[now].style.zIndex=0;
+                lia[j].style.zIndex=15;
+                lib[now].classList.remove("hot");
+                lib[j].classList.add("hot");
+                next=now=j;
+            }
+            else if(j<now){
+                lia[now].style.zIndex=0;
+                lia[j].style.zIndex=15;
+                lib[now].classList.remove("hot");
+                lib[j].classList.add("hot");
+                next=now=j;
+            }
+        }
+    }
+
+
+
+
+
+
+
+    window.onscroll=function(){
+        sh=document.body.scrollTop || document.documentElement.scrollTop;
+        let yfive=document.querySelector(".yfive")
+        if(sh>150){
+            yfive.style.display="block";
+        }
+        else if(sh==0){
+             yfive.style.display="none";
+        }
+        yfive.onclick=function(){
+			animate(document.body,{scrollTop:0},2000);
+			animate(document.documentElement,{scrollTop:0},2000);
+		}
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
